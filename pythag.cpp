760398cbc7ee4b664cfbb5329869c50fae37eba1
jared <77174400+JaredEpicMan43@users.noreply.github.com>
2021-01-08 15:37:49 -0600

    Add files via upload

---
+++ pythag.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+int main () {
+float a;
+float b;
+float c;
+
+std::cout << "enter a leg: ";
+std::cin >> a;
+std::cout << "enter leg two: ";
+std::cin >> b;
+c = sqrt(pow(a,2) + pow(b,2));
+std::cout << c;
+return 0;
+}
+//c = sqrt(pow(a,2) + pow(b,2));
